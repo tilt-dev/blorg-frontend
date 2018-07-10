ddc5ba89a3506f2211dce5171af97f71d3953c66
pmt <patriciatorvalds@gmail.com>
2018-07-10 15:20:22 -0400

    Add Random function to talk to blorgly

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"flag"
 	"io/ioutil"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 const endptPong = "/pong"
+const endptRand = "/random"
 
 var backend = flag.String("backendAddr", "http://localhost:8080", "address of the blorg backend server")
+var blorglybackend = flag.String("blorglyBackendAddr", "http://localhost:8082", "address of blorgly backend server")
 
 func main() {
 	flag.Parse()
@@ -21,6 +24,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Hello)
 	r.HandleFunc("/ping", Ping)
+	r.HandleFunc("/random", Random)
 	http.Handle("/", r)
 	fmt.Println("Starting up on 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
@@ -52,6 +56,34 @@ func Ping(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(500)
 		fmt.Fprintf(w,
 			"Expected 'pong'; backend server said '%s' (%s)\n", strings.TrimSpace(string(body)), resp.Status)
+		return
+	}
+
+	fmt.Fprintf(w, "SUCCESS! ﾍ(=￣∇￣)ﾉ\n")
+}
+
+func Random(w http.ResponseWriter, req *http.Request) {
+	// TODO: Will want to be more careful concat'ing base + endpt in future
+	// see http://bit.ly/2lFlOCq
+	url := fmt.Sprintf("%s%s", *blorglybackend, endptRand)
+	resp, err := http.Get(url)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Request to backend server resulted in error: %v\n", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Could not read backend server resp. with error: %v\n", err)
+		return
+	}
+
+	if resp.StatusCode != 200 || strings.TrimSpace(string(body)) != "random" {
+		w.WriteHeader(500)
+		fmt.Fprintf(w,
+			"Expected 'random'; backend server said '%s' (%s)\n", strings.TrimSpace(string(body)), resp.Status)
 		return
 	}
 
